2c25a2601861ae5072d096cc32038816ebc50e54
Abbas Oveissi <abbas.oveissi@gmail.com>
2016-06-08 17:22:15 +0430

    ezafe kardan mesal e be request ha

--- app/src/main/java/ir/oveissi/simplevolleysample/data/RequestRepository.java
+++ app/src/main/java/ir/oveissi/simplevolleysample/data/RequestRepository.java
@@ -4,6 +4,9 @@ import android.content.Context;
 
 import com.android.volley.Request;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import ir.oveissi.simplevolleysample.data.jsonmodel.DetailMovie;
 import ir.oveissi.simplevolleysample.data.jsonmodel.TmpMovies;
 
@@ -31,6 +34,27 @@ public class RequestRepository {
         CustomGsonRequest sr = new CustomGsonRequest<DetailMovie>(url, Request.Method.GET, DetailMovie.class, null, listener);
         NetworkManager.getInstance(mContext).requestQueue.add(sr);
     }
+
+
+    //iin method serfan jahate mesale hast va webservice hamchin darkhasti ra poshtibani nemikonad
+    public void getMovieByPostReuqest(String movie_id,final MyNetworkListener<DetailMovie> listener) {
+        String url = NetworkManager.prefixURL + "?i="+movie_id;
+        Map<String,String> params=new HashMap<>();
+        params.put("plot","short");
+        params.put("r","json");
+        CustomGsonRequest sr = new CustomGsonRequest<DetailMovie>(url, Request.Method.POST, DetailMovie.class, null, listener);
+        NetworkManager.getInstance(mContext).requestQueue.add(sr);
+    }
+
+
+    //iin method serfan jahate mesale hast va webservice hamchin darkhasti ra poshtibani nemikonad
+    public void getMovieByAuth(String movie_id,final MyNetworkListener<DetailMovie> listener) {
+        String url = NetworkManager.prefixURL + "?i="+movie_id+"&plot=short&r=json";
+        CustomGsonRequest sr = new CustomGsonRequest<DetailMovie>(url, Request.Method.GET, DetailMovie.class, null, listener);
+        sr.setToken("inja token ro gharar midiim");
+        NetworkManager.getInstance(mContext).requestQueue.add(sr);
+    }
+
 
 
 }
